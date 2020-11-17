0d43165735072036a4d84281479b1a628e959f81
Revincx <revincx@outlook.com>
2020-11-17 13:46:12 +0800

    推送新版本

--- js/introduction.js
+++ js/introduction.js
@@ -6,6 +6,22 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 });
+onSizeChanged();
+
+$(window).resize(function(){
+	onSizeChanged();
+});
+
+function onSizeChanged(){
+	if($(window).width()<900){
+		$('#videos').removeClass('justify-content-between')
+		$('#videos').addClass('justify-content-center')
+	}
+	else{
+		$('#videos').addClass('justify-content-between')
+		$('#videos').removeClass('justify-content-center')
+	}
+};
 
 //$('navLinks').click(function(){
 //	$(this).toggleClass('active');
